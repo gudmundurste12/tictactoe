9dad58f6b6d529c1fab2cafaf3116f7af797c12f
gudmundurste12 <gudmundurste12@ru.is>
2014-12-01 12:34:36 +0000

    Updated the dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
+# export PATH=$PATH:/usr/local/bin
 echo Cleaning...
 rm -rf ./dist
+
+echo Resolving npm and bower dependencies
+npm install
+bower install
 
 echo Building app
 grunt
 
 cp ./Dockerfile ./dist/
-
 cd dist
 npm install --production
 
