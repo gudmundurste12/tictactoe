fce6fafdbb640b996720ce5755f551bcad3fb8dc
gudmundurste12 <gudmundurste12@ru.is>
2014-12-01 18:30:38 +0000

    Edited the dockerbuild and now this works

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# export PATH=$PATH:/usr/local/bin
+
 echo Cleaning...
 rm -rf ./dist
 
 echo Resolving npm and bower dependencies
 npm install
 bower install
+
+echo Updating path
+export PATH=$PATH:/usr/local/bin
 
 echo Building app
 grunt
@@ -15,6 +18,6 @@ cd dist
 npm install --production
 
 echo Building docker image
-docker build -t gulli/tictactoe .
+docker build -t gudmundurste12/tictactoe .
 
 echo "Done"
