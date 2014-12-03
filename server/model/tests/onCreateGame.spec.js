ab0ea5b1fa2b422cbf5b54920ed772c7e5d711be
gudmundurste12 <gudmundurste12@ru.is>
2014-12-03 14:13:08 +0000

    Wrote a new test case that check whether or not the history is updated correctly after creating a game

--- server/model/tests/onCreateGame.spec.js
+++ server/model/tests/onCreateGame.spec.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var tictactoeEventHandler = require('../tictactoeEventHandler');
 
 describe("Create game:", function(){
-	it("Given an empty history, history should only contain the GameCreated event", function(){
+	it("Given an empty history, handleEvend should only return the GameCreated event", function(){
 		//Assign
 		var history = [];
 		
@@ -26,7 +26,33 @@ describe("Create game:", function(){
 
 		//Assert
 		should(result.length).be.exactly(1);
-		//should(tictactoeEventHandler.history.length).be.exactly(1);
 		should(JSON.stringify(result)).be.exactly(JSON.stringify(then));
+	});
+});
+
+
+describe("Create game:", function(){
+	it("Given an empty history, history should only contain the GameCreated event", function(){
+		//Assign
+		var history = [];
+		
+		var when = {
+			commandName: "CreateGame",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		};
+		
+		var then = [
+		{
+			eventName: "GameCreated",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		}];
+
+		//Act
+		var result = tictactoeEventHandler(history).handleEvent(when);
+
+		//Assert
+		should(tictactoeEventHandler.history.length).be.exactly(1);
 	});
 });
