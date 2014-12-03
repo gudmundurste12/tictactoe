a893a025b331a071ec1ee6c29e37aea0cff07dfb
gudmundurste12 <gudmundurste12@ru.is>
2014-12-03 18:37:15 +0000

    Added a test: the creator of a game should not be able to join the game

--- server/model/tests/onJoinGame.spec.js
+++ server/model/tests/onJoinGame.spec.js
@@ -89,7 +89,7 @@ describe("Join game: ", function(){
 	});
 
 
-	it("A game should be joinable if it has been created and not joined", function(){
+	it("A game should not be joinable by the user who created the game", function(){
 		//Arrange
 		var history = [
 		{
@@ -106,8 +106,50 @@ describe("Join game: ", function(){
 
 		var then = [
 		{
-			eventName: "GameJoined",
+			eventName: "BadCommand",
+			event: {
+				commandName: "JoinGame",
+				userName: "Gvendurst",
+				timeStamp: "2014-12-02T11:34:29"
+			},
+			history: [
+			{
+				eventName: "GameCreated",
+				userName: "Gvendurst",
+				timeStamp: "2014-12-02T11:29:29"
+			}],
+			details: "This user created the game"
+		}];
+
+		//Act
+		var eventHandler = tictactoeEventHandler(history);
+		var result = eventHandler.handleEvent(when);
+
+		//Assert
+		should(result.length).be.exactly(1);
+		should(JSON.stringify(result)).be.exactly(JSON.stringify(then));
+	});
+
+
+	it("A game should be joinable if it has been created and not joined", function(){
+		//Arrange
+		var history = [
+		{
+			eventName: "GameCreated",
 			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		}];
+
+		var when = {
+			commandName: "JoinGame",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:34:29"
+		};
+
+		var then = [
+		{
+			eventName: "GameJoined",
+			userName: "Gvendurst2",
 			timeStamp: "2014-12-02T11:34:29"
 		}];
 
