eed5cb91c9fdf23e1416b3a6745e135864dac673
gudmundurste12 <gudmundurste12@ru.is>
2014-12-03 21:19:55 +0000

    Players can now mark the cells in the grid

--- server/model/tictactoeState.js
+++ server/model/tictactoeState.js
@@ -6,17 +6,14 @@ module.exports = function(history){
 
 	tictactoeState.history = history;
 
-	tictactoeState.getGrid = function(){
-		var grid = [
-		["","",""],
-		["","",""],
-		["","",""]
-		];
+	tictactoeState.currentPlayer = null;
+	tictactoeState.players = [];
 
-		//TODO: Update the grid according to the history
-
-		return grid;
-	};
+	tictactoeState.grid = [
+	["","",""],
+	["","",""],
+	["","",""]
+	];
 
 	tictactoeState.joinable = function(){
 		var returnValue = true;
@@ -38,6 +35,44 @@ module.exports = function(history){
 		});
 		return returnValue;
 	}	
+
+	tictactoeState.getGrid = function(){
+		return tictactoeState.grid;
+	};
+
+	tictactoeState.updateGrid = function(theEvent){
+		if(theEvent instanceof Array){
+			_.each(theEvent, function(currentEvent){
+				tictactoeState.updateGrid(currentEvent);
+			});
+		}
+		else{
+			if(theEvent.eventName === "GameCreated"){
+				tictactoeState.currentPlayer = theEvent.userName;
+				tictactoeState.players[0] = theEvent.userName;
+			}
+			else if(theEvent.eventName === "GameJoined"){
+				tictactoeState.currentPlayer = theEvent.userName;
+				tictactoeState.players[1] = theEvent.userName;
+			}
+			else if(theEvent.eventName === "MoveMade"){
+				if(theEvent.userName === tictactoeState.players[0]){
+					tictactoeState.grid[theEvent.cell.x][theEvent.cell.y] = "x";
+				}
+				else{
+					tictactoeState.grid[theEvent.cell.x][theEvent.cell.y] = "o";
+				}
+			}
+		}
+	}
+
+	tictactoeState.setHistory = function(theHistory){
+		_.each(theHistory, function(currentEvent){
+			tictactoeState.updateGrid(currentEvent);
+		})
+	};
+
+	tictactoeState.setHistory(history);
 
 	return tictactoeState;
 }
