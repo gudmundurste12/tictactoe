9b837b4a5eb5b800011feef5986244b5c52cf6e0
gudmundurste12 <gudmundurste12@ru.is>
2014-12-03 15:09:13 +0000

    Added a test that checks whether or not the grid will be empty when the game is created

--- server/model/tests/onCreateGame.spec.js
+++ server/model/tests/onCreateGame.spec.js
@@ -2,6 +2,7 @@ var should = require('should');
 var _ = require('lodash');
 
 var tictactoeEventHandler = require('../tictactoeEventHandler');
+var tictactoeState = require('../tictactoeState');
 
 describe("Create game:", function(){
 	it("Given an empty history, handleEvent should only return the GameCreated event", function(){
@@ -28,10 +29,7 @@ describe("Create game:", function(){
 		should(result.length).be.exactly(1);
 		should(JSON.stringify(result)).be.exactly(JSON.stringify(then));
 	});
-});
 
-
-describe("Create game:", function(){
 	it("Given a non-empty history, a BadCommand event should be returned", function(){
 		//Assign
 		var history = [
@@ -70,5 +68,33 @@ describe("Create game:", function(){
 		//Assert
 		should(result.length).be.exactly(1);
 		should(JSON.stringify(result)).be.exactly(JSON.stringify(then));
+	});
+
+
+	it("Given an empty history and the GameCreated event, the gamestate should be an empty grid", function(){
+		//Assign
+		var history = [];
+		
+		var when = {
+			commandName: "CreateGame",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		};
+		
+		var gridAfter = [
+		["","",""],
+		["","",""],
+		["","",""]
+		];
+
+		//Act
+		//TODO: Wrap this functionality somehow, some planning required
+		var result = tictactoeEventHandler(history).handleEvent(when);
+		var newHistory = history;
+		newHistory.push(result);
+		var gameState = tictactoeState(newHistory);
+
+		//Assert
+		should(gameState.getGrid().be.exactly(gridAfter));
 	});
 });
