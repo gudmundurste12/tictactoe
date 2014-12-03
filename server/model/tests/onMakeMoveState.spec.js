158cea07f03587a6f849f68dce53065449fde707
gudmundurste12 <gudmundurste12@ru.is>
2014-12-03 21:42:16 +0000

    Added a unit test: Players should not be able to pick a cell outside of the grid

--- server/model/tests/onMakeMoveState.spec.js
+++ server/model/tests/onMakeMoveState.spec.js
@@ -142,4 +142,86 @@ describe("tictactoeState on MakeMove", function(){
 		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
 		should(gameState.currentPlayer).be.exactly("Gvendurst2");
 	});
+
+	it("The players should not be able to pick a cell that is not on the grid", function(){
+		//Arrange
+		var history = [
+		{
+			eventName: "GameCreated",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		},
+		{
+			eventName: "GameJoined",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:34:29"
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: -1,
+				y: -1
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: -1,
+				y: 0
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 3,
+				y: 3
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: -1,
+				y: 3
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 3,
+				y: -1
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 3,
+				y: 0
+			}
+		}];
+		
+		var gridAfter = [
+		["","",""],
+		["","",""],
+		["","",""]
+		];
+
+		//Act
+		var gameState = tictactoeState(history);
+
+		//Assert
+		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
+		should(gameState.currentPlayer).be.exactly("Gvendurst");
+	});
 });
