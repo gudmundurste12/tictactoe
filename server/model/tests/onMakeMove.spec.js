1587ff369d728e71ac93b30984a95349205106f3
gudmundurste12 <gudmundurste12@ru.is>
2014-12-04 12:17:34 +0000

    Added a test: No moves should be made if a game has not been created

--- server/model/tests/onMakeMove.spec.js
+++ server/model/tests/onMakeMove.spec.js
@@ -4,7 +4,38 @@ var tictactoeEventHandler = require('../tictactoeEventHandler');
 var tictactoeState = require('../tictactoeState');
 
 describe("Make move:", function(){
-	it("", function(){
-		//TODO: Test the state or the event handler?
+	it("No move should be made if the history is empty", function(){
+		//Arrange
+		var history = [];
+
+		var when = {
+			commandName: "MakeMove",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		};
+
+		var then = [
+		{
+			eventName: "BadCommand",
+			event: {
+				commandName: "MakeMove",
+				userName: "Gvendurst",
+				timeStamp: "2014-12-02T11:29:29"
+			},
+			history: [],
+			message: "Game has not been created"
+		}];
+
+		//Act
+		var eventHandler = tictactoeEventHandler(history);
+		var result = eventHandler.handleEvent(when);
+
+		//Assert
+		should(result.length).be.exactly(1);
+		should(JSON.stringify(result)).be.exactly(JSON.stringify(then));
+	});
+
+	it("Should return a MoveMade event", function(){
+		
 	});
 });
