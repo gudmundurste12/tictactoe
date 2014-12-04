763fd0a542814b6a79b075ea788f1738c0e4b5cc
gudmundurste12 <gudmundurste12@ru.is>
2014-12-04 11:10:46 +0000

    Added a unit test: The game should be unresolved if no move has been made

--- server/model/tests/onWinConditionsState.spec.js
+++ server/model/tests/onWinConditionsState.spec.js
@@ -118,4 +118,37 @@ describe("tictactoeState on Win or Draw:", function(){
 		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
 	});
 
+
+	it("The game should be uresolved if no move has been made", function(){
+		//Arrange
+		var history = [
+		{
+			eventName: "GameCreated",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		},
+		{
+			eventName: "GameJoined",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:34:29"
+		}];
+		
+		var gridAfter = [
+		["","",""],
+		["","",""],
+		["","",""]
+		];
+
+		var gameStatusAfter = {
+			status: "Unresolved"
+		};
+
+		//Act
+		var gameState = tictactoeState(history);
+
+		//Assert
+		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
+		should(gameState.currentPlayer).be.exactly("Gvendurst");
+		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
+	});
 });
