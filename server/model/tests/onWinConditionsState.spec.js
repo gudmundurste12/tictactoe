c5a66a69dee597342f8b93141b1cd192b864451a
gudmundurste12 <gudmundurste12@ru.is>
2014-12-04 12:14:46 +0000

    Diagonal events are now tested

--- server/model/tests/onWinConditionsState.spec.js
+++ server/model/tests/onWinConditionsState.spec.js
@@ -417,4 +417,137 @@ describe("tictactoeState on Win or Draw:", function(){
 		should(gameState.currentPlayer).be.exactly("Gvendurst2");
 		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
 	});
+
+	
+	it("Diagonal wins should be possible and moves should not be possible after a game has been won", function(){
+		//Arrange
+		var history = [
+		{
+			eventName: "GameCreated",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:29:29"
+		},
+		{
+			eventName: "GameJoined",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:34:29"
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 0,
+				y: 0
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 2,
+				y: 0
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 2,
+				y: 2
+			}
+		},
+		{
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 0,
+				y: 2
+			}
+		}];
+		
+		var gridAfter = [
+		["x","","o"],
+		["","",""],
+		["o","","x"]
+		];
+
+		var gameStatusAfter = {
+			status: "Unresolved"
+		};
+
+		//Act
+		var gameState = tictactoeState(history);
+
+		//Assert
+		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
+		should(gameState.currentPlayer).be.exactly("Gvendurst");
+		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
+
+		var extraEvent = {
+			eventName: "MoveMade",
+			userName: "Gvendurst",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 1,
+				y: 1
+			}
+		};
+
+		history.push(extraEvent);
+
+		var gridAfter = [
+		["x","","o"],
+		["","x",""],
+		["o","","x"]
+		];
+
+		var gameStatusAfter = {
+			status: "Win",
+			userName: "Gvendurst"
+		};
+
+		//Act
+		gameState = tictactoeState(history);
+
+		//Assert
+		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
+		should(gameState.currentPlayer).be.exactly("Gvendurst2");
+		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
+
+
+		extraEvent = {
+			eventName: "MoveMade",
+			userName: "Gvendurst2",
+			timeStamp: "2014-12-02T11:39:29",
+			cell: {
+				x: 1,
+				y: 0
+			}
+		};
+
+		history.push(extraEvent);
+
+		var gridAfter = [
+		["x","","o"],
+		["","x",""],
+		["o","","x"]
+		];
+
+		var gameStatusAfter = {
+			status: "Win",
+			userName: "Gvendurst"
+		};
+
+		//Act
+		gameState = tictactoeState(history);
+
+		//Assert
+		should(JSON.stringify(gameState.getGrid())).be.exactly(JSON.stringify(gridAfter));
+		should(gameState.currentPlayer).be.exactly("Gvendurst2");
+		should(JSON.stringify(gameState.getStatus())).be.exactly(JSON.stringify(gameStatusAfter));
+	});
 });
