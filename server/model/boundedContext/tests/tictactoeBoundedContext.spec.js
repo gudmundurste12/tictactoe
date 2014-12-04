f10b7c479b379a9ab29d648bc24c32ff8554a8c1
gudmundurste12 <gudmundurste12@ru.is>
2014-12-04 14:40:08 +0000

    Wrote a unit test to tictactoeBoundedContext.spec.js

--- server/model/boundedContext/tests/tictactoeBoundedContext.spec.js
+++ server/model/boundedContext/tests/tictactoeBoundedContext.spec.js
@@ -2,6 +2,7 @@ var should = require("should");
 var _ = require("lodash");
 
 describe("", function(){
+	//Arrange
 	var eventStoreCalledWithId = undefined;
 	var eventStoreStub = {
 		setHistory: function(id){
@@ -9,12 +10,24 @@ describe("", function(){
 		}
 	};
 
-
+	var commandHandlerCalledWithCommand = undefined;
 	var commandHandlerStub = {
 		handleCommand: function(command){
-
+			commandHandlerCalledWithCommand = command;
 		}
 	};
 
-	
+	var textCommand = {
+		command: "TestCommand",
+		gameId: "1"
+	};
+
+	var boundedContext = require("../tictactoeBoundedContext.js");
+
+	//Act
+	boundedContext.handleCommand(testCommand);
+
+	//Assert
+	should(eventStoreCalledWithId).be.exactly("1");
+	should(commandHandlerCalledWithCommand).eql(command);
 });
