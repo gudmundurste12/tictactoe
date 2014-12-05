edeee64b67003a55806a6eff0b1e7d554e27bc71
gudmundurste12 <gudmundurste12@ru.is>
2014-12-05 09:22:33 +0000

    Testing if createGameController returns a non empty array

--- server/api/tictactoeApi/createGame/tests/onCreateGame.controller.spec.js
+++ server/api/tictactoeApi/createGame/tests/onCreateGame.controller.spec.js
@@ -4,19 +4,34 @@ var request = require("supertest");
 
 
 describe("Controller: creating game", function(){
-	it("The result should be a JSON array", function(done){
+	it("A CreateGame command should give a GameCreated event", function(done){
+		var command = {
+			commandName: "CreateGame",
+			userName: "Gvendurst",
+			gameId: "1",
+			timeStamp: "2014-12-02T11:29:29"
+		};
+
+		var event = {
+			eventName: "GameCreated",
+			userName: "Gvendurst",
+			gameId: "1",
+			timeStamp: "2014-12-02T11:29:29"
+		};
+
 		var req = request(app);
 
 		req
 		.post("/api/createGame")
 		.type("json")
-		.send({})
+		.send(command)
 		.end(function(err, res){
 			console.log("Done");
 			if(err){
 				console.log("err: " + err);
 			}
-			res.body.should.be.exactly("HERP");
+			should(res.body).be.instanceof(Array);
+			should(res.body.length).not.be.exactly(0);
 			done();
 		});
 	});
