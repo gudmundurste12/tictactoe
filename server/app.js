53892763532aab684c2213dd3139a187ef87f711
gudmundurste12 <gudmundurste12@ru.is>
2014-12-16 19:19:20 +0000

    mongoStore is not used in the development environment anymore

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ server.listen(config.port, config.ip, function () {
 
 console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
 
-if(process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'development'){
+if(process.env.NODE_ENV === 'prod'){
 	console.log("Using mongoStore");
 	app.eventStore = require('./eventStore/mongoStore/mongoStore')();
 }
